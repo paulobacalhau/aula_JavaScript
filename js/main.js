2f877f217b6772479e9b78bb36154224b9e04284
PAULO-MICROLINE\dell <desenvolvimento.microline@gmail.com>
2020-05-13 17:04:45 -0400

    Primeira aula de JavaScript

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// alert ('Meu primeiro JS');
+var nome = "Paulo ";
+var idade = 58;
+// alert (nome + " tem " + idade + " anos.");
+var frase = "Japão é o melhor time do mundo";
+console.log(nome);
+console.log(nome + idade);
+console.log(frase.replace("Japão", "Brasil"));
+console.log(frase.toUpperCase());
+
