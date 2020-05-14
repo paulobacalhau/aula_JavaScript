8cd2c53f480f79343c9a9a27e3fa94b38e11c608
PAULO-MICROLINE\dell <desenvolvimento.microline@gmail.com>
2020-05-13 23:44:31 -0400

    Segunda aula de JavaScript

--- js/main.js
+++ js/main.js
@@ -2,9 +2,28 @@
 var nome = "Paulo ";
 var idade = 58;
 // alert (nome + " tem " + idade + " anos.");
+/*
 var frase = "Japão é o melhor time do mundo";
 console.log(nome);
 console.log(nome + idade);
 console.log(frase.replace("Japão", "Brasil"));
 console.log(frase.toUpperCase());
+var lista =["maçã", "pera", "laranja"];
+lista.push("uva");
+console.log(lista);
+console.log(lista.length);
+lista.pop();
+console.log(lista);
+console.log(lista[1]);
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join("-"));
+var fruta = {nome: "maçã", cor: "vermelha"};
+console.log(fruta.nome);
+*/
+// alert(fruta.cor);
 
+var frutas = [{nome: "maçã", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
+console.log(frutas);
+//alert(frutas[1].cor);
